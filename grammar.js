18eedd83c50e666753e6764d59995940b87d8528
Alyssa Carter <trillionalyssa@gmail.com>
2024-09-04 12:09:43 -0400

    TypeScript lsp

--- grammar.js
+++ grammar.js
@@ -1,19 +1,34 @@
 /// <reference types="tree-sitter-cli/dsl" />
 // @ts-check
 
+/**
+ * @param {RuleOrLiteral} body
+ */
+function barred(body) {
+  return seq('{{', body, '}}')
+}
+
+/**
+ * @param {RuleOrLiteral} tag
+ * @param {RuleOrLiteral} body
+ */
+function fancy_barred(tag, body) {
+  return barred(seq(tag, body))
+}
+
 module.exports = grammar({
   name: 'handlebars_html',
   rules: {
     source_file: $ => repeat1($._content),
     _content: $ => choice($.literal_text, $.handlebars, $.handleblock, $.macro, $.partial),
-    literal_text: $ => /[^{]+/,
-    handlebars: $ => seq('{{', $.expression,'}}'),
+    literal_text: _$ => /[^{]+/,
+    handlebars: $ => barred($.expression),
     expression: $ => choice($.identifier),
-    identifier: $ => /[a-zA-Z]+/,
-    block_start: $ => seq('{{', '#', $.identifier, '}}'),
-    block_end: $ => seq('{{', '/', $.identifier, '}}'),
+    identifier: _$ => /[a-zA-Z]+/,
+    block_start: $ => fancy_barred('#', $.identifier),
+    block_end: $ => fancy_barred('/', $.identifier),
     handleblock: $ => seq($.block_start, repeat($._content), $.block_end),
-    macro_start: $ => seq('{{', '*', 'inline', $.expression, '}}'),
+    macro_start: $ => fancy_barred('*', seq($.identifier, $.expression)),
     macro: $ => seq($.macro_start, repeat($._content), $.block_end),
     partial_start: $ => seq('{{', '>', $.expression, '}}'),
     partial: $ => seq($.partial_start, repeat($._content), $.block_end),
